6a470efc9f9f8a0f156bc7f908fb498c9ba5d1f5
Ronald Bett <rbett_ctr@utilita.co.uk>
2024-03-25 10:07:01 +0300

    chore: clrk

--- Agc.GoodShepherd.Infrastructure/DataAccess/AppDbContext.cs
+++ Agc.GoodShepherd.Infrastructure/DataAccess/AppDbContext.cs
@@ -1,21 +1,19 @@
-using Cards.Application.Interfaces;
-using Cards.Domain.Models;
-using Cards.Infrastructure.DataAccess.EntityConfigurations;
+using Agc.GoodShepherd.Application.Interfaces;
+using Agc.GoodShepherd.Domain.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Cards.Infrastructure.DataAccess
+namespace Agc.GoodShepherd.Infrastructure.DataAccess
 {
     public class AppDbContext : IdentityDbContext<ApplicationUser>, IAppDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         { }
-        
-        public DbSet<Card> Cards { get; set; }
+
+        public DbSet<Congregant> Congregants { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.ApplyConfiguration(new CardsEntityTypeConfiguration());
             base.OnModelCreating(modelBuilder);
         }
     }
